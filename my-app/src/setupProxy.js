4549245200b228b01ccea34a9cbed6c452d6a7a5
rhino-ty <okko8522@gmail.com>
2022-12-08 13:52:06 +0900

    feat: proxy로 다중 api 불러오기 성공!

--- my-app/src/setupProxy.js
+++ my-app/src/setupProxy.js
@@ -1,11 +1,22 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+// module.exports = function (app) {
+//   app.use(
+//     "/api", // proxy가 필요한 path prameter를 입력.
+//     createProxyMiddleware({
+//       target: "http://localhost:3080", // 타겟이 되는 api url를 입력.
+//       changeOrigin: true, // 대상 서버 구성에 따라 호스트 헤더가 변경되도록 설정하는 부분.
+//     })
+//   );
+// };
+
 module.exports = function (app) {
   app.use(
-    "/api", // proxy가 필요한 path prameter를 입력.
+    ["/api1", "/api2"],
     createProxyMiddleware({
       target: "http://localhost:3080", // 타겟이 되는 api url를 입력.
       changeOrigin: true, // 대상 서버 구성에 따라 호스트 헤더가 변경되도록 설정하는 부분.
+      router: { "/api2": "http://localhost:3070" },
     })
   );
 };
